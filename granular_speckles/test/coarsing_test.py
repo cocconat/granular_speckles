1704f454673aeb1a5ce39d113c9f6b238fc33174
cocconat <g33k@paranoici.org>
2019-01-10 03:36:20 +0100

    test for matrix coarsing

---
+++ granular_speckles/test/coarsing_test.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# This file belongs to DWGranularSpeckles project.
+# The software is realeased with MIT license. 
+# coding: utf-8
+
+from granular_speckles import CoarseMatrix
+import numpy as np
+a=np.random.random((10,14))
+corse=CoarseMatrix(2,a.shape)
+reduced=corse.coarseMatrix(a)
+print "la matrice di aprtenza è la stessa? {}".format((corse.matrix==a).all())
+buona=corse.matrixReduction()
+if (buona == reduced).all():
+	print "vaivaivai"
+else:
+	print buona
